6924e56e9753b2564d1659e94c993ec1393ac1ec
Annette Wöhler <annettewoehler@gmx.de>
2024-02-26 19:55:21 +0100

    + Add basic styles & import font

--- styles.js
+++ styles.js
@@ -1,14 +1,65 @@
 import { createGlobalStyle } from "styled-components";
+import { Nunito_Sans } from "next/font/google";
+
+export const nunito_sans = Nunito_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default createGlobalStyle`
+  :root {
+    /* Color Styles */
+    --color_white: #fff;
+    --color_body: #f8f8f9;
+    --color_light-grey: #d8d8d8;
+    --color_grey: #838688;
+    --color_night: #5b5f62;
+    --color_midnight: #434648;
+    --color_light-orange: #feb776;
+    --color_orange: #fea554;
+    --color_light-red: #f9847c;
+    --color_red: #ff6e63;
+    --color_sunset: linear-gradient(to top right, #fea554, #ff5e62);
+
+    /* Font Styles */
+    --font-family: ${nunito_sans.style.fontFamily};
+
+    --font_heading-1: 700 1.25rem/1.35 var(--font-family);
+    --font_heading-2: 700 1.125rem/1.56 var(--font-family);
+    --font_heading-3: 900 0.875rem/1.36 var(--font-family);
+    --font_heading-category: 900 1.25rem/1.35 var(--font-family);
+
+    --font_body: 300 1rem/1.5 var(--font-family);
+    --font_info: 600 0.875rem/1.43 var(--font-family);
+    --font_label: 700 0.875rem/1.43 var(--font-family);
+    --font_footer: 700 0.75rem/1.33 var(--font-family);
+
+    --font_button-primary: 800 0.8125rem/1.38 var(--font-family);
+    --font_button-secondary: 800 0.75rem/1.33 var(--font-family);
+    
+    /* Effect Styles */
+    --border-radius_input: 3px;
+    --border-radius_card: 6px;
+    --border-radius_button: 30px;
+    --border-radius_round-button: 50px;
+
+    --shadow_card: 0px 2px 12px 0px rgba(91, 95, 98, 0.2);
+    --shadow_round-button: 0px 2px 4px 0px rgba(91, 95, 98, 0.24);
+
+    --transition_button: 0.3s;
+  }
+
   *,
   *::before,
   *::after {
+    margin: 0;
+    padding: 0;
     box-sizing: border-box;
   }
 
   body {
-    margin: 0;
-    font-family: system-ui;
+    background: var(--color_body);
+    font: var(--font_body);
+    color: var(--color_grey)
   }
 `;
