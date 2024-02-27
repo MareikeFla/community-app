476295c869aad92c33d23b0ae086618554c70453
MareikeFla <mwidderich@gmail.com>
2024-02-27 11:27:26 +0100

    Add footer and styling

---
+++ components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import { StyledFooter, TextFooter } from "./Footer.styles";
+
+export default function Footer() {
+  return (
+    <StyledFooter>
+      <TextFooter>CREATED 2024</TextFooter>
+    </StyledFooter>
+  );
+}
