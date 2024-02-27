69f89b8f1ae24148e6f02d525651c0793f8a80cf
MareikeFla <mwidderich@gmail.com>
2024-02-27 11:28:12 +0100

    Add Layout component and styling

---
+++ components/Layout/Layout.js
@@ -0,0 +1,13 @@
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
+import { MainContent } from "./Layout.styled";
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <Header />
+      <MainContent> {children}</MainContent>
+      <Footer />
+    </>
+  );
+}
