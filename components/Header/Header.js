68557f8f01c44f2cd7ca0b89fe27a2d7a860f3e3
MareikeFla <mwidderich@gmail.com>
2024-02-27 11:27:05 +0100

    Add header and styling

---
+++ components/Header/Header.js
@@ -0,0 +1,19 @@
+import Logo from "public/logo.svg";
+import { HeaderBackground, LogoHeader, HeaderCutout } from "./Header.styles";
+
+export default function Header() {
+  return (
+    <HeaderBackground>
+      <LogoHeader>
+        <Logo aria-label="Pin&Join Logo" />
+      </LogoHeader>
+      <HeaderCutout
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 100 100"
+        preserveAspectRatio="none"
+      >
+        <polygon fill="#f8f8f9" points="0,0 0,100 100,100" />
+      </HeaderCutout>
+    </HeaderBackground>
+  );
+}
