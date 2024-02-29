9893755f4a28f142995c4fa76c7eaa684a6b83ae
Annette Wöhler <annettewoehler@gmx.de>
2024-02-29 18:17:04 +0100

    * Update font weights

--- styles.js
+++ styles.js
@@ -24,14 +24,14 @@ export default createGlobalStyle`
     /* Font Styles */
     --font-family: ${nunito_sans.style.fontFamily};
 
-    --font_heading-1: 700 1.25rem/1.35 var(--font-family);
+    --font_heading-1: 600 1.25rem/1.35 var(--font-family);
     --font_heading-2: 600 1.125rem/1.56 var(--font-family);
     --font_heading-3: 900 0.875rem/1.36 var(--font-family);
     --font_heading-category: 900 1.25rem/1.35 var(--font-family);
 
-    --font_body: 300 1rem/1.5 var(--font-family);
+    --font_body: 200 1rem/1.5 var(--font-family);
     --font_info: 300 0.875rem/1.43 var(--font-family);
-    --font_label: 700 0.875rem/1.43 var(--font-family);
+    --font_label: 600 0.875rem/1.43 var(--font-family);
     --font_footer: 700 0.75rem/1.33 var(--font-family);
 
     --font_button-primary: 800 0.8125rem/1.38 var(--font-family);
@@ -63,6 +63,5 @@ export default createGlobalStyle`
     color: var(--color_grey);
     min-height: 100vh;
     position: relative;
-
   }
 `;
