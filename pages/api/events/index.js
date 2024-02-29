37118a665bf0e5c3b0528601628bc75ae23d786d
MareikeFla <mwidderich@gmail.com>
2024-02-29 14:24:37 +0100

    await connect

--- pages/api/events/index.js
+++ pages/api/events/index.js
@@ -2,7 +2,7 @@ import dbConnect from "@/db/connect";
 import Event from "@/db/models/Event";
 
 export default async function handler(request, response) {
-  dbConnect();
+  await dbConnect();
 
   if (request.method === "GET") {
     try {
