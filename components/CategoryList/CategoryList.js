829a18462c338c250b754811486741d18d800954
Bruno <bruno.dill@protonmail.com>
2024-03-02 15:01:29 +0100

    + add data fetching for categories

--- components/CategoryList/CategoryList.js
+++ components/CategoryList/CategoryList.js
@@ -1,18 +1,32 @@
-import { mainCategories } from "@/lib/mainCategories";
 import {
   CategoryListStyled,
   CategoryListHeader,
   CategoryListContainer,
+  CategoryLink,
 } from "./CategoryList.styled";
 import CategoryCard from "../CategoryCard/CategoryCard";
+import useSWR from "swr";
+import Loading from "../Loading/Loading";
+import FetchingError from "../FetchingError/FetchingError";
 
 export default function CategoryList() {
+  const { data: categories, isLoading, error } = useSWR("/api/categories");
+
+  if (isLoading) {
+    return <Loading />;
+  }
+  if (error) {
+    return <FetchingError />;
+  }
+
   return (
     <CategoryListContainer>
       <CategoryListHeader>Was interessiert dich?</CategoryListHeader>
       <CategoryListStyled>
-        {mainCategories.map((category) => (
-          <CategoryCard key={category.slug} category={category} />
+        {categories.map((category) => (
+          <CategoryLink key={category._id} href={`/${category.slug}`}>
+            <CategoryCard category={category} />
+          </CategoryLink>
         ))}
       </CategoryListStyled>
     </CategoryListContainer>
