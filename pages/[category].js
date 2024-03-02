c3e5062c12933fc69dada32aeafb4de8ed4b4f9d
Bruno <bruno.dill@protonmail.com>
2024-03-02 14:59:55 +0100

    + add dynamic routing for category pages

---
+++ pages/[category].js
@@ -0,0 +1,26 @@
+import BackButton from "@/components/BackButton/BackButton";
+import EventList from "@/components/EventList/EventList";
+import FetchingError from "@/components/FetchingError/FetchingError";
+import Loading from "@/components/Loading/Loading";
+import { useRouter } from "next/router";
+import useSWR from "swr";
+
+export default function HomePage() {
+  const router = useRouter();
+
+  const { data, isLoading, error } = useSWR(`/api/${router.query.category}`);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+  if (error) {
+    return <FetchingError />;
+  }
+
+  return (
+    <>
+      <BackButton />
+      <EventList events={data} />
+    </>
+  );
+}
