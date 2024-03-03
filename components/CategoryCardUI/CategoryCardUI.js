8787f56cc1dab607d831e89f065d093d909e75fb
Bruno <bruno.dill@protonmail.com>
2024-03-03 11:33:59 +0100

    + created new component for UI rendering

---
+++ components/CategoryCardUI/CategoryCardUI.js
@@ -0,0 +1,28 @@
+import {
+  CategoryCardContainer,
+  CategoryImage,
+  CategoryInfo,
+  CategoryInfoContainer,
+  CategoryInfoHeading,
+} from "./CategoryCardUI.styled";
+
+export default function CategoryCardUI({ category, filteredEvents }) {
+  const { imageSource, imageAlt, title } = category;
+  return (
+    <CategoryCardContainer>
+      <CategoryImage
+        src={imageSource}
+        alt={imageAlt}
+        height={126}
+        width={183}
+      />
+      <CategoryInfoContainer>
+        <CategoryInfoHeading>{title}</CategoryInfoHeading>
+        <CategoryInfo>
+          {filteredEvents.length}{" "}
+          {filteredEvents.length === 1 ? "Event" : "Events"}
+        </CategoryInfo>
+      </CategoryInfoContainer>
+    </CategoryCardContainer>
+  );
+}
