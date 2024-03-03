01c1db56b5ed3d9ddf3ff85139743eee6678e112
Bruno <bruno.dill@protonmail.com>
2024-03-03 11:33:10 +0100

    - removed UI rendering for single responibility

--- components/CategoryCard/CategoryCard.js
+++ components/CategoryCard/CategoryCard.js
@@ -1,13 +1,7 @@
 import useSWR from "swr";
-import {
-  CategoryCardContainer,
-  CategoryImage,
-  CategoryInfo,
-  CategoryInfoContainer,
-  CategoryInfoHeading,
-} from "./CategoryCard.styled";
 import Loading from "../Loading/Loading";
 import FetchingError from "../FetchingError/FetchingError";
+import CategoryCardUI from "../CategoryCardUI/CategoryCardUI";
 
 export default function CategoryCard({ category }) {
   const { data, isLoading, error } = useSWR(`api/${category.slug}`);
@@ -19,20 +13,5 @@ export default function CategoryCard({ category }) {
     return <FetchingError />;
   }
 
-  return (
-    <CategoryCardContainer>
-      <CategoryImage
-        src={category.imageSource}
-        alt={category.imageAlt}
-        height={126}
-        width={183}
-      />
-      <CategoryInfoContainer>
-        <CategoryInfoHeading>{category.title}</CategoryInfoHeading>
-        <CategoryInfo>
-          {data.length} {data.length === 1 ? "Event" : "Events"}
-        </CategoryInfo>
-      </CategoryInfoContainer>
-    </CategoryCardContainer>
-  );
+  return <CategoryCardUI filteredEvents={data} category={category} />;
 }
