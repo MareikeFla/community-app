c56b9dfc13b405b39720824010e684fdda8e1d8d
MareikeFla <mwidderich@gmail.com>
2024-03-04 12:57:56 +0100

    Add useRef hook, useState with modal infos and openModal function and pass ist to components

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,51 @@
 import Layout from "@/components/Layout/Layout";
 import GlobalStyle from "../styles";
 import { SWRConfig } from "swr";
+import { useState, useRef } from "react";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function App({ Component, pageProps }) {
+  const hook = useRef();
+
+  const [modalInfo, setModalInfo] = useState({
+    message: "",
+    textButtonClose: "",
+    textButtonConfirm: "",
+    onConfirm: () => {},
+    onClose: () => {},
+  });
+
+  const openModal = ({
+    message,
+    textButtonClose,
+    textButtonConfirm,
+    onConfirm,
+  }) => {
+    setModalInfo({
+      message,
+      textButtonClose,
+      textButtonConfirm,
+      onConfirm,
+      onClose,
+    });
+    hook.current.showModal();
+  };
+  const onClose = function () {
+    hook.current.close();
+  };
+
   return (
     <>
       <GlobalStyle />
       <Layout>
         <SWRConfig value={{ fetcher }}>
-          <Component {...pageProps} />
+          <Component
+            {...pageProps}
+            openModal={openModal}
+            modalInfo={modalInfo}
+            hook={hook}
+          />
         </SWRConfig>
       </Layout>
     </>
