fd7871c98ccf521bd97eddc55159caa26a1a1829
MareikeFla <mwidderich@gmail.com>
2024-03-05 14:34:34 +0100

    Add api for searchTerm

---
+++ pages/api/search/[searchTerm]/index.js
@@ -0,0 +1,24 @@
+import dbConnect from "@/db/connect";
+import Event from "@/db/models/Event";
+
+export default async function handler(request, response) {
+  await dbConnect();
+  const { searchTerm } = request.query;
+
+  if (request.method === "GET") {
+    try {
+      const filteredEvents = await Event.find({
+        $text: {
+          $search: searchTerm,
+        },
+      });
+      if (!filteredEvents) {
+        return response.status(404).json({ status: "Not Found" });
+      }
+      return response.status(200).json(filteredEvents);
+    } catch (error) {
+      console.error(error);
+      return response.status(400).json({ error: error.message });
+    }
+  }
+}
