fd635fc92e2ca14c2775267676fcf6a93d7b08ce
Annette Wöhler <annettewoehler@gmx.de>
2024-03-05 12:03:49 +0100

    + Add Navigation component

---
+++ components/Navigation/Navigation.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import {
+  NavigationContainer,
+  NavIcon,
+  NavMenu,
+  NavLink,
+} from "./Navigation.styled";
+
+export default function Navigation() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <NavigationContainer>
+      <NavIcon onClick={toggleMenu} isOpen={isOpen}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </NavIcon>
+      <NavMenu isOpen={isOpen}>
+        <ul>
+          <li>
+            <NavLink href="/" onClick={toggleMenu}>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink href="events/new" onClick={toggleMenu}>
+              Event erstellen
+            </NavLink>
+          </li>
+        </ul>
+      </NavMenu>
+    </NavigationContainer>
+  );
+}
