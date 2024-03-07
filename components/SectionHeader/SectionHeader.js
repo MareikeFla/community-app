027cccb10ae042264c8da6efa8cf195f31a541b0
Bruno <bruno.dill@protonmail.com>
2024-03-07 22:33:32 +0100

    + add SectionHeader component

---
+++ components/SectionHeader/SectionHeader.js
@@ -0,0 +1,5 @@
+import { Header } from "./SectionHeader.styled";
+
+export default function SectionHeader({ children }) {
+  return <Header>{children}</Header>;
+}
---
+++ components/SectionHeader/SectionHeader.styled.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Header = styled.h3`
+  font: var(--font_heading-2);
+  margin-bottom: 2rem;
+`;
