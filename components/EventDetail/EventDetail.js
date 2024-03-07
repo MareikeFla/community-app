9a24518f948ce3d54ba5f1adde34e7867465ee7c
Bruno <bruno.dill@protonmail.com>
2024-03-07 22:33:07 +0100

    + add CommentSection to EventDetails

--- components/EventDetail/EventDetail.js
+++ components/EventDetail/EventDetail.js
@@ -1,5 +1,6 @@
 import BackButton from "../BackButton/BackButton";
 import CategoryTag from "../CategoryTag/CategoryTag";
+import CommentSection from "../CommentSection/CommentSection";
 import DeleteEventButton from "../DeleteEventButton/DeleteEventButton";
 import Map from "../Map";
 import {
@@ -37,51 +38,55 @@ export default function EventDetail({ event, showDeleteModal }) {
     organization,
     links,
     category,
+    comments,
   } = event;
   const { organizationName, organizationContact } = organization;
   const { street, houseNumber, zip, city, latitude, longitude } = location;
 
   return (
-    <Card>
-      <BackButton />
-      <DeleteEventButton id={_id} showDeleteModal={showDeleteModal} />
-      <EventName>{eventName}</EventName>
-      <Description>{longDescription}</Description>
-      <InfoWrapper>
-        <InfoTitle>Beginn</InfoTitle>
-        <Info>
-          {start.date}, {start.time} Uhr
-        </Info>
-        <InfoTitle>Ende</InfoTitle>
-        <Info>
-          {end.date}, {end.time} Uhr
-        </Info>
-        <InfoTitle>Ort</InfoTitle>
-        <Info>
-          {zip && street && houseNumber && city
-            ? `${street} ${houseNumber}, ${zip}, ${city}`
-            : "Online"}
-        </Info>
-        <InfoTitle>Kosten</InfoTitle>
-        <Info>{costs}</Info>
-        <InfoTitle>Veranstalter</InfoTitle>
-        <Info>{organizationName}</Info>
-        <InfoTitle>Kontakt</InfoTitle>
-        <Info>{organizationContact}</Info>
-        <InfoTitle>Weitere Infos</InfoTitle>
-        <LinkList>
-          {links.map((link, index) => (
-            <ListItem key={index}>
-              <ListItemMarker>&raquo;</ListItemMarker>
-              <ListItemLink href={link.url} target="blank">
-                {link.linkDescription}
-              </ListItemLink>
-            </ListItem>
-          ))}
-        </LinkList>
-        {longitude && latitude && <Map event={event} />}
-      </InfoWrapper>
-      <CategoryTag category={category} />
-    </Card>
+    <>
+      <Card>
+        <BackButton />
+        <DeleteEventButton id={_id} showDeleteModal={showDeleteModal} />
+        <EventName>{eventName}</EventName>
+        <Description>{longDescription}</Description>
+        <InfoWrapper>
+          <InfoTitle>Beginn</InfoTitle>
+          <Info>
+            {start.date}, {start.time} Uhr
+          </Info>
+          <InfoTitle>Ende</InfoTitle>
+          <Info>
+            {end.date}, {end.time} Uhr
+          </Info>
+          <InfoTitle>Ort</InfoTitle>
+          <Info>
+            {zip && street && houseNumber && city
+              ? `${street} ${houseNumber}, ${zip}, ${city}`
+              : "Online"}
+          </Info>
+          <InfoTitle>Kosten</InfoTitle>
+          <Info>{costs}</Info>
+          <InfoTitle>Veranstalter</InfoTitle>
+          <Info>{organizationName}</Info>
+          <InfoTitle>Kontakt</InfoTitle>
+          <Info>{organizationContact}</Info>
+          <InfoTitle>Weitere Infos</InfoTitle>
+          <LinkList>
+            {links.map((link, index) => (
+              <ListItem key={index}>
+                <ListItemMarker>&raquo;</ListItemMarker>
+                <ListItemLink href={link.url} target="blank">
+                  {link.linkDescription}
+                </ListItemLink>
+              </ListItem>
+            ))}
+          </LinkList>
+          {longitude && latitude && <Map event={event} />}
+        </InfoWrapper>
+        <CategoryTag category={category} />
+      </Card>
+      <CommentSection id={_id} comments={comments} />
+    </>
   );
 }
