46f8ba46ac3dfbcc2fa016e858602256f517afb7
MareikeFla <mwidderich@gmail.com>
2024-03-07 23:02:11 +0100

    Implement flexible search based on keyword

--- pages/api/search/[searchTerm]/index.js
+++ pages/api/search/[searchTerm]/index.js
@@ -1,38 +1,28 @@
 import dbConnect from "@/db/connect";
 import Event from "@/db/models/Event";
+import { searchPipline, autocompletePipeline } from "@/lib/mongoDBPipelines";
 
 export default async function handler(request, response) {
   await dbConnect();
-  const { searchTerm } = request.query;
+
+  //Deconstructore searchterm and keyword from query
+
+  const {
+    query: { searchTerm },
+  } = request;
+  const {
+    query: { keyword },
+  } = request;
+
+  // Check if query is for search or for autocomplete and set pipeline function accordingly
+
+  const pipeline = keyword === "search" ? searchPipline : autocompletePipeline;
 
   if (request.method === "GET") {
     try {
-      const filteredEvents = await Event.aggregate([
-        {
-          $search: {
-            index: "fullText",
-            text: {
-              query: searchTerm,
-              path: {
-                wildcard: "*",
-              },
-              fuzzy: { maxEdits: 1 },
-            },
-          },
-        },
-        {
-          $sort: {
-            score: { $meta: "searchScore" },
-          },
-        },
-        {
-          $addFields: {
-            textScore: { $meta: "searchScore" },
-          },
-        },
-      ]);
+      const result = await Event.aggregate(pipeline(searchTerm));
 
-      return response.status(200).json(filteredEvents);
+      return response.status(200).json(result);
     } catch (error) {
       console.error(error);
       return response.status(400).json({ error: error.message });
