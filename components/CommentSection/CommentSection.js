6139901e63817e0636e7a5c466da6cd83275430a
Bruno <bruno.dill@protonmail.com>
2024-03-08 08:30:56 +0100

    + add mock user data to function

--- components/CommentSection/CommentSection.js
+++ components/CommentSection/CommentSection.js
@@ -3,21 +3,19 @@ import CommentForm from "../CommentForm/CommentForm";
 import CommentList from "../CommentList/CommentList";
 import { CommentCard } from "./CommentSection.styled";
 import SectionHeader from "../SectionHeader/SectionHeader";
+import { user } from "@/lib/user";
 
 export default function CommentSection({ id, comments }) {
   async function handlePostComment(data) {
-    //this is just a placeholder until we get users
-    const sendData = {
-      userName: "Jane Doe",
-      userImageURL: "/assets/images/activism.jpg",
-      text: data,
-    };
     const response = await fetch(`/api/events/${id}`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(sendData),
+      body: JSON.stringify({
+        ...user,
+        text: data,
+      }),
     });
 
     if (response.ok) {
@@ -27,7 +25,9 @@ export default function CommentSection({ id, comments }) {
 
   return (
     <section>
-      <SectionHeader>{comments.length} Kommentare</SectionHeader>
+      <SectionHeader>
+        {comments.length} {comments.length === 1 ? "Kommentar" : "Kommentare"}
+      </SectionHeader>
       <CommentCard>
         <CommentForm onPostComment={handlePostComment} />
         <CommentList comments={comments} />
