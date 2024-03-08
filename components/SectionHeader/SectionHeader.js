b1e5cb778152368c9b8335837f0e11af80f3a21f
Annette Wöhler <annettewoehler@gmx.de>
2024-03-08 12:47:05 +0100

    * Rename component

--- components/SectionHeader/SectionHeader.js
+++
@@ -1,5 +0,0 @@
-import { Header } from "./SectionHeader.styled";
-
-export default function SectionHeader({ children }) {
-  return <Header>{children}</Header>;
-}
--- components/SectionHeader/SectionHeader.styled.js
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-
-export const Header = styled.h3`
-  font: var(--font_heading-2);
-  margin-bottom: 2rem;
-`;
