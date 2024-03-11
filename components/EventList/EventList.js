e77303d800d066c2ece4b5526453e27962c18d66
MareikeFla <mwidderich@gmail.com>
2024-03-11 10:12:11 +0100

    Let event list accept a heading prop

--- components/EventList/EventList.js
+++ components/EventList/EventList.js
@@ -1,13 +1,14 @@
 import EventPreview from "../EventPreview/EventPreview";
 import { EventListWrapper, EventDetailsLink } from "./EventList.styled";
 
-export default function EventList({ events = [], isSorted }) {
+export default function EventList({ events = [], isSorted, heading }) {
   if (!isSorted) {
     events.sort((a, b) => new Date(a.start.date) - new Date(b.start.date));
   }
 
   return (
     <EventListWrapper>
+      {heading ? <h2>{heading}</h2> : null}
       {events.map((event) => (
         <EventDetailsLink key={event._id} href={`/events/${event._id}`}>
           <EventPreview event={event} />
