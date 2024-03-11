d77210f9b7a1dd353c2677aaae4878bc65893e99
MareikeFla <mwidderich@gmail.com>
2024-03-11 13:58:10 +0100

    /events/new will pass the title and the function to update a datase to EventFormCard

--- pages/events/new.js
+++ pages/events/new.js
@@ -1,5 +1,25 @@
-import NewEventCard from "@/components/NewEventCard/NewEventCard";
+import EventFormCard from "@/components/EventForm/EventFormCard";
+import { mutate } from "swr";
 
 export default function NewCard() {
-  return <NewEventCard />;
+  const handleNewEvent = async (data) => {
+    const response = await fetch("/api/events", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (response.ok) {
+      mutate();
+    }
+  };
+
+  return (
+    <EventFormCard
+      title={"Erstelle ein neues Event"}
+      updateDatabase={handleNewEvent}
+    />
+  );
 }
