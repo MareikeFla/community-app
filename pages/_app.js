aaa174592301ff658cd2d841d78bcd7453a7fc1c
MareikeFla <mwidderich@gmail.com>
2024-03-13 10:46:24 +0100

    Add context and Confirmation modal to App component

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import Layout from "@/components/Layout/Layout";
 import GlobalStyle from "../styles";
 import { SWRConfig } from "swr";
-import { useState, useRef } from "react";
 import React from "react";
-const fetcher = (...args) => fetch(...args).then((res) => res.json());
 import { StyledToastContainer } from "@/components/Toast/Toast.styled";
+import { ModalProvider } from "@/lib/useModal";
+import { ConfirmationModal } from "@/components/ConfirmationModal/ConfirmationModal";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function App({ Component, pageProps }) {
   return (
@@ -13,7 +15,10 @@ export default function App({ Component, pageProps }) {
       <StyledToastContainer />
       <Layout>
         <SWRConfig value={{ fetcher }}>
-          <Component {...pageProps} />
+          <ModalProvider>
+            <Component {...pageProps} />
+            <ConfirmationModal />
+          </ModalProvider>
         </SWRConfig>
       </Layout>
     </>
