9b4999a9d37b231447157387dff25d1e02829e21
Annette Wöhler <annettewoehler@gmx.de>
2024-03-14 09:33:35 +0100

    + Add wrapper and header components

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -2,14 +2,25 @@ import Navigation from "../Navigation/Navigation";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import BackToTopButton from "../BackToTopButton/BackToTopButton";
-import { MainContent } from "./Layout.styled";
+import { LogoHeader, MainContent, Wrapper } from "./Layout.styled";
+import SearchLink from "../Search/SearchLink";
+import Link from "next/link";
+import Logo from "public/logo.svg";
 
 export default function Layout({ children }) {
   return (
     <>
-      <Navigation />
       <Header />
-      <MainContent>{children}</MainContent>
+      <Wrapper>
+        <Navigation />
+        <LogoHeader>
+          <Link href="/">
+            <Logo aria-label="Pin&Join Logo" />
+          </Link>
+        </LogoHeader>
+        <SearchLink />
+        <MainContent>{children}</MainContent>
+      </Wrapper>
       <Footer />
       <BackToTopButton />
     </>
