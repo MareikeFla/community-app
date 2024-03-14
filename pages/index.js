88bf41fc5235231d2831fc5fcee3e1712e30d61b
MareikeFla <mwidderich@gmail.com>
2024-03-14 15:04:45 +0100

    Load events and categories on main page based on Context states

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,16 @@
 import WelcomeCard from "@/components/WelcomeCard/WelcomeCard";
 import CategoryList from "@/components/CategoryList/CategoryList";
 import EventList from "@/components/EventList/EventList";
-import FetchingError from "@/components/FetchingError/FetchingError";
-import Loading from "@/components/Loading/Loading";
-import useSWR from "swr";
+import { Data } from "@/lib/DataContext";
 
 export default function HomePage() {
-  const {
-    data: events,
-    isLoading: isLoadingEvents,
-    error: errorEvents,
-  } = useSWR("/api/events");
-  const {
-    data: categories,
-    isLoading: isLoadingCategories,
-    error: errorCategories,
-  } = useSWR("/api/categories");
-
-  if (isLoadingEvents || isLoadingCategories) {
-    return <Loading />;
-  }
-  if (errorEvents || errorCategories) {
-    return <FetchingError />;
-  }
+  const { events, categories } = Data();
 
   return (
     <>
       <WelcomeCard />
       <CategoryList categories={categories} />
-      <EventList events={events} isSorted={false} title={"Aktuelle Events"} />
+      <EventList events={events} title={"Aktuelle Events"} />
     </>
   );
 }
