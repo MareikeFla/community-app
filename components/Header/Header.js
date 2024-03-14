8ab7c2c5fabc9edf89ed7cab6c652d50e78771b5
Annette Wöhler <annettewoehler@gmx.de>
2024-03-14 09:29:52 +0100

    - Remove redundant components

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,17 +1,8 @@
-import Link from "next/link";
-import Logo from "public/logo.svg";
-import { HeaderBackground, LogoHeader, HeaderCutout } from "./Header.styles";
-import SearchLink from "../Search/SearchLink";
+import { HeaderBackground, HeaderCutout } from "./Header.styles";
 
 export default function Header() {
   return (
     <HeaderBackground>
-      <SearchLink />
-      <LogoHeader>
-        <Link href="/">
-          <Logo aria-label="Pin&Join Logo" />
-        </Link>
-      </LogoHeader>
       <HeaderCutout
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 100 100"
