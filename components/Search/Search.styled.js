6fce29050a483420d4647aa84825bce7a8963968
Annette Wöhler <annettewoehler@gmx.de>
2024-03-14 09:34:49 +0100

    * Update SearchLink styles

--- components/Search/Search.styled.js
+++ components/Search/Search.styled.js
@@ -49,15 +49,8 @@ export const SearchBar = styled.input`
 // SearchLink
 
 export const SearchLinkStyled = styled(Link)`
-  display: flex;
-  justify-content: center;
-  align-items: center;
   position: absolute;
-  top: 1.1875rem;
+  top: 1.125rem;
   right: 1rem;
-  background-color: var(--color_white) 20%;
-  border: none;
-  border-radius: var(--border-radius_square-button);
-  box-shadow: var(--shadow_button);
   cursor: pointer;
 `;
