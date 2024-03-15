c6e04d6be936daecde4b879eb40fe58f11ad2ce5
Annette Wöhler <annettewoehler@gmx.de>
2024-03-15 10:15:14 +0100

    * Update background color

--- components/CategoryTag/CategoryTag.styled.js
+++ components/CategoryTag/CategoryTag.styled.js
@@ -4,7 +4,7 @@ export const Button = styled.span`
   font: var(--font_button-secondary);
   color: var(--color_white);
   text-transform: uppercase;
-  background-color: var(--color_red);
+  background-color: ${(props) => `var(--color_${props.color})`};
   border: none;
   border-radius: var(--border-radius_button);
   padding: 0.375rem 0.625rem 0.3125rem;
