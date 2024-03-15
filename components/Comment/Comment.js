9c13a132b6a7fe9175ed0eb22085063ebe1b6e46
Bruno <bruno.dill@protonmail.com>
2024-03-15 10:08:00 +0100

    + add handleLikeComment to Comment component

--- components/Comment/Comment.js
+++ components/Comment/Comment.js
@@ -8,11 +8,32 @@ import {
   ProfilePicture,
 } from "./Comment.styled";
 import LikeButton from "../LikeButton/LikeButton";
+import { useState } from "react";
+import { mutate } from "swr";
 
 export default function Comment({ comment }) {
-  const { userImageURL, userName, text, creationDate } = comment;
+  const { userImageURL, userName, text, creationDate, isLiked, _id } = comment;
+  const [checkIfIsLiked, setCheckIfIsLiked] = useState(isLiked);
 
   const timeElapsed = getTimeElapsed(creationDate);
+
+  async function handleLikeComment(comment) {
+    const response = await fetch(`/api/comments/${_id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        _id,
+        isLiked: !checkIfIsLiked,
+      }),
+    });
+
+    if (response.ok) {
+      setCheckIfIsLiked(!checkIfIsLiked);
+      mutate();
+    }
+  }
 
   return (
     <CommentContainer>
@@ -27,7 +48,10 @@ export default function Comment({ comment }) {
           {userName} <CommentTime>·{creationDate && timeElapsed}</CommentTime>
         </CommentHeader>
         <CommentBody>{text}</CommentBody>
-        <LikeButton />
+        <LikeButton
+          onLikeComment={handleLikeComment}
+          checkIfIsLiked={checkIfIsLiked}
+        />
       </CommentText>
     </CommentContainer>
   );
