471e80040ff49fb5905a03f3a0f0eb04230d4144
MareikeFla <mwidderich@gmail.com>
2024-03-18 23:47:06 +0100

    NewEvent: Replace existing data fetch with ne createEvent function

--- pages/events/new.js
+++ pages/events/new.js
@@ -1,27 +1,13 @@
 import EventFormContainer from "@/components/EventForm/EventFormContainer";
-import { mutate } from "swr";
+import { useData } from "@/lib/useData";
 
 export default function NewEvent() {
-  const handleNewEvent = async (data) => {
-    const response = await fetch("/api/events", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-
-    if (response.ok) {
-      const newEvent = await response.json();
-      mutate();
-      return newEvent.id;
-    }
-  };
+  const { createEvent } = useData();
 
   return (
     <EventFormContainer
       title={"Erstelle ein neues Event"}
-      updateDatabase={handleNewEvent}
+      updateDatabase={(eventData) => createEvent(eventData)}
     />
   );
 }
