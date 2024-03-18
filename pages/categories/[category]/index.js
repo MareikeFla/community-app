3afe8affcece21ee6539b10a92dc143c1ca062f8
MareikeFla <mwidderich@gmail.com>
2024-03-18 23:13:55 +0100

    CategoryPage: Replace data fetching for events with new filterEventsByCategoryID function

--- pages/categories/[category]/index.js
+++ pages/categories/[category]/index.js
@@ -5,39 +5,35 @@ import FetchingError from "@/components/FetchingError/FetchingError";
 import Loading from "@/components/Loading/Loading";
 import MessageCard from "@/components/MessageCard/MessageCard";
 import { useRouter } from "next/router";
-import useSWR from "swr";
 import { useData } from "@/lib/useData";
 
 export default function CategoryPage() {
   const router = useRouter();
   const { category } = router.query;
+  const { filterEventsByCategoryID } = useData();
 
   const { categories, isLoadingCategories, errorCategories } =
     useData().fetchedCategories;
 
-  const {
-    data: events,
-    isLoading: eventLoading,
-    error: eventError,
-  } = useSWR(category ? `/api/categories/${category}` : null);
-
-  if (isLoadingCategories || eventLoading) {
+  if (isLoadingCategories) {
     return <Loading />;
   }
-  if (errorCategories || eventError) {
+  if (errorCategories) {
     return <FetchingError />;
   }
 
   const selectedCategory = categories.find((cat) => cat.slug === category);
 
+  const filteredEvents = filterEventsByCategoryID(selectedCategory._id);
+
   return (
     <>
       <BackButton />
       {selectedCategory && <CategoryHeader category={selectedCategory} />}
-      {events.length === 0 ? (
+      {filteredEvents.length === 0 ? (
         <MessageCard>Keine Events gefunden...</MessageCard>
       ) : (
-        <EventList events={events} isSorted={false} />
+        <EventList events={filteredEvents} isSorted={false} />
       )}
     </>
   );
