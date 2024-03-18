ebf9537823084cf4054bf4049a58a54d577b67f6
MareikeFla <mwidderich@gmail.com>
2024-03-19 00:07:20 +0100

    DeleteEventButton: Replace existing fetch with new deleteEvent function

--- components/DeleteEventButton/DeleteEventButton.js
+++ components/DeleteEventButton/DeleteEventButton.js
@@ -2,27 +2,19 @@ import { useRouter } from "next/router";
 import { DeleteButton } from "./DeleteEventButton.styled";
 import Image from "next/image";
 import { useModal } from "@/lib/useModal";
+import { useData } from "@/lib/useData";
 
 export default function DeleteEventButton({ id }) {
   const router = useRouter();
+  const { deleteEvent } = useData();
+
   const { showModal } = useModal();
   const modalContent = {
     message: "Dieses Event wirklich löschen?",
     textButtonCancel: "Abbrechen",
     textButtonConfirm: "Löschen",
-    onConfirm: handleDelete,
+    onConfirm: () => deleteEvent(id, router),
   };
-
-  async function handleDelete() {
-    const response = await fetch(`/api/events/${id}`, {
-      method: "DELETE",
-    });
-    if (response.ok) {
-      router.push("/");
-      return true;
-    }
-    return false;
-  }
 
   return (
     <DeleteButton
