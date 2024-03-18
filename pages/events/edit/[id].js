eb15467159514e56a65e1d04439f2adcede4fa67
MareikeFla <mwidderich@gmail.com>
2024-03-18 23:36:38 +0100

    EditEvent: Replace existig fetch with new getEventByID function

--- pages/events/edit/[id].js
+++ pages/events/edit/[id].js
@@ -1,19 +1,20 @@
 import EventFormContainer from "@/components/EventForm/EventFormContainer";
-import useSWR from "swr";
 import { mutate } from "swr";
 import { useRouter } from "next/router";
 import Loading from "@/components/Loading/Loading";
 import FetchingError from "@/components/FetchingError/FetchingError";
+import { useData } from "@/lib/useData";
 
 export default function EditEvent() {
   const router = useRouter();
   const { id } = router.query;
-  const { data: event, isLoading, error } = useSWR(`/api/events/${id}`);
+  const { getEventByID } = useData();
+  const { event, isLoadingEvent, errorEvent } = getEventByID(id);
 
-  if (isLoading) {
+  if (isLoadingEvent) {
     return <Loading />;
   }
-  if (error) {
+  if (errorEvent) {
     return <FetchingError />;
   }
   async function handleEditEvent(eventData) {
