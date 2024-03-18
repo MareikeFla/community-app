03b2ef5ada0313916b6e3ab141ac54264460bbdd
MareikeFla <mwidderich@gmail.com>
2024-03-18 23:53:14 +0100

    EditEvent: replace existing fetch with new updateEvent function

--- pages/events/edit/[id].js
+++ pages/events/edit/[id].js
@@ -8,7 +8,7 @@ import { useData } from "@/lib/useData";
 export default function EditEvent() {
   const router = useRouter();
   const { id } = router.query;
-  const { getEventByID } = useData();
+  const { getEventByID, updateEvent } = useData();
   const { event, isLoadingEvent, errorEvent } = getEventByID(id);
 
   if (isLoadingEvent) {
@@ -17,24 +17,11 @@ export default function EditEvent() {
   if (errorEvent) {
     return <FetchingError />;
   }
-  async function handleEditEvent(eventData) {
-    const response = await fetch(`/api/events/${id}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(eventData),
-    });
 
-    if (response.ok) {
-      mutate();
-      return response;
-    }
-  }
   return (
     <EventFormContainer
       title={"Event bearbeiten"}
-      updateDatabase={handleEditEvent}
+      updateDatabase={(eventData) => updateEvent(eventData, id)}
       event={event}
     />
   );
