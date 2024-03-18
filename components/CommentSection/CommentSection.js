39b2ed3b2475c07bb0d5b67764ad79596176e20a
MareikeFla <mwidderich@gmail.com>
2024-03-19 00:24:31 +0100

    CommentSection: Accept mutate function as a prop and add it to addComment function

--- components/CommentSection/CommentSection.js
+++ components/CommentSection/CommentSection.js
@@ -1,11 +1,13 @@
-import { mutate } from "swr";
 import { user } from "@/lib/user";
 import { CommentCard } from "./CommentSection.styled";
 import SectionTitle from "../SectionTitle/SectionTitle";
 import CommentForm from "../CommentForm/CommentForm";
 import CommentList from "../CommentList/CommentList";
+import { useData } from "@/lib/useData";
 
-export default function CommentSection({ id, comments }) {
+export default function CommentSection({ id, comments, mutate }) {
+  const { addComment } = useData();
+
   async function handlePostComment(data) {
     const response = await fetch(`/api/events/${id}`, {
       method: "POST",
@@ -36,7 +38,9 @@ export default function CommentSection({ id, comments }) {
         {comments.length} {comments.length === 1 ? "Kommentar" : "Kommentare"}
       </SectionTitle>
       <CommentCard>
-        <CommentForm onPostComment={handlePostComment} />
+        <CommentForm
+          onPostComment={(comment) => addComment(id, comment, mutate)}
+        />
         <CommentList comments={sortedComments} />
       </CommentCard>
     </section>
