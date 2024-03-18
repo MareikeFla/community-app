e35fafc414106b89c9fc01def9e5e09fa68adfbc
MareikeFla <mwidderich@gmail.com>
2024-03-19 00:22:59 +0100

    EventDetailPage: Event specific mutateEvent function from getEventByID function and add the function to event object passed to EventDetail

--- pages/events/[id]/index.js
+++ pages/events/[id]/index.js
@@ -1,6 +1,5 @@
 import { useRouter } from "next/router";
 import EventDetail from "@/components/EventDetail/EventDetail";
-import useSWR from "swr";
 import Loading from "@/components/Loading/Loading";
 import FetchingError from "@/components/FetchingError/FetchingError";
 import { useData } from "@/lib/useData";
@@ -8,7 +7,9 @@ import { useData } from "@/lib/useData";
 export default function EventDetailPage() {
   const router = useRouter();
   const { getEventByID } = useData();
-  const { event, isLoadingEvent, errorEvent } = getEventByID(router.query.id);
+  const { event, isLoadingEvent, errorEvent, mutateEvent } = getEventByID(
+    router.query.id
+  );
 
   if (isLoadingEvent) {
     return <Loading />;
@@ -16,6 +17,8 @@ export default function EventDetailPage() {
   if (errorEvent) {
     return <FetchingError />;
   }
+
+  event.mutate = mutateEvent;
 
   return (
     <>
