cf15846489f98b1f8754c1eb7e724e89eac19ab6
MareikeFla <mwidderich@gmail.com>
2024-03-18 21:39:36 +0100

    CategoryPage: Replace fetch with categories provided by context.

--- pages/categories/[category]/index.js
+++ pages/categories/[category]/index.js
@@ -6,10 +6,17 @@ import Loading from "@/components/Loading/Loading";
 import MessageCard from "@/components/MessageCard/MessageCard";
 import { useRouter } from "next/router";
 import useSWR from "swr";
+import { useData } from "@/lib/useData";
 
 export default function CategoryPage() {
   const router = useRouter();
   const { category } = router.query;
+
+  const {
+    data: categories,
+    isLoading: categoriesLoading,
+    error: categoriesError,
+  } = useData().categories;
 
   const {
     data: events,
@@ -17,16 +24,10 @@ export default function CategoryPage() {
     error: eventError,
   } = useSWR(category ? `/api/categories/${category}` : null);
 
-  const {
-    data: categories,
-    isLoading: categoryLoading,
-    error: categoryError,
-  } = useSWR("/api/categories");
-
-  if (categoryLoading || eventLoading) {
+  if (categoriesLoading || eventLoading) {
     return <Loading />;
   }
-  if (categoryError || eventError) {
+  if (categoriesError || eventError) {
     return <FetchingError />;
   }
 
