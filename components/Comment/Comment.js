536bbca906e71eab06f74325698bf6346b7de430
MareikeFla <mwidderich@gmail.com>
2024-03-19 18:58:57 +0100

    Comment: Replace existing function

--- components/Comment/Comment.js
+++ components/Comment/Comment.js
@@ -9,30 +9,14 @@ import {
 } from "./Comment.styled";
 import LikeButton from "../LikeButton/LikeButton";
 import { useState } from "react";
+import { useData } from "@/lib/useData";
 
 export default function Comment({ comment, mutate }) {
   const { userImageURL, userName, text, creationDate, isLiked, _id } = comment;
   const [checkIfIsLiked, setCheckIfIsLiked] = useState(isLiked);
+  const { updateComment } = useData();
 
   const timeElapsed = getTimeElapsed(creationDate);
-
-  async function handleLikeComment() {
-    const response = await fetch(`/api/comments/${_id}`, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        _id,
-        isLiked: !checkIfIsLiked,
-      }),
-    });
-
-    if (response.ok) {
-      setCheckIfIsLiked(!checkIfIsLiked);
-      mutate();
-    }
-  }
 
   return (
     <CommentContainer>
@@ -48,7 +32,9 @@ export default function Comment({ comment, mutate }) {
         </CommentHeader>
         <CommentBody>{text}</CommentBody>
         <LikeButton
-          onLikeComment={handleLikeComment}
+          onLikeComment={() =>
+            updateComment(_id, checkIfIsLiked, setCheckIfIsLiked, mutate)
+          }
           checkIfIsLiked={checkIfIsLiked}
         />
       </CommentText>
