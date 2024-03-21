783186a60ace58b1fd71f9d9481d5679a73404e9
Bruno <bruno.dill@protonmail.com>
2024-03-21 19:19:42 +0100

    *make field required

--- db/models/User.js
+++ db/models/User.js
@@ -4,10 +4,9 @@ const { Schema } = mongoose;
 
 const userSchema = new Schema({
   creationDate: { type: Date, default: new Date() },
-  name: { type: String },
-  email: { type: String },
-  image: { type: String },
-  events: { type: Array },
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  image: { type: String, required: true },
 });
 
 const User = mongoose.models.User || mongoose.model("User", userSchema);
