91d76d912e1a25e0695704074bbca93db3b539bc
Bruno <bruno.dill@protonmail.com>
2024-03-22 11:50:30 +0100

    * render MessageCard if user is not logged in

--- pages/events/new.js
+++ pages/events/new.js
@@ -1,13 +1,26 @@
+import ArrowButton from "@/components/ArrowButton/ArrowButton";
 import EventFormContainer from "@/components/EventForm/EventFormContainer";
+import MessageCard from "@/components/MessageCard/MessageCard";
 import { useData } from "@/lib/useData";
+import { signIn, useSession } from "next-auth/react";
 
 export default function NewEvent() {
   const { createEvent } = useData();
+  const { data: session } = useSession();
 
   return (
-    <EventFormContainer
-      title={"Erstelle ein neues Event"}
-      updateDatabase={(eventData) => createEvent(eventData)}
-    />
+    <>
+      {!session ? (
+        <MessageCard>
+          <p>Bitte melde dich an, um Events zu erstellen:</p>
+          <ArrowButton onClick={() => signIn()}>Jetzt Anmelden</ArrowButton>
+        </MessageCard>
+      ) : (
+        <EventFormContainer
+          title={"Erstelle ein neues Event"}
+          updateDatabase={(eventData) => createEvent(eventData)}
+        />
+      )}
+    </>
   );
 }
