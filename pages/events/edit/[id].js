466a5626724e2ea7c31604908450dc8fe842d490
Bruno <bruno.dill@protonmail.com>
2024-03-22 12:23:45 +0100

    * protect edit page if logged in user is not event owner

--- pages/events/edit/[id].js
+++ pages/events/edit/[id].js
@@ -3,12 +3,18 @@ import { useRouter } from "next/router";
 import Loading from "@/components/Loading/Loading";
 import FetchingError from "@/components/FetchingError/FetchingError";
 import { useData } from "@/lib/useData";
+import { useSession } from "next-auth/react";
+import MessageCard from "@/components/MessageCard/MessageCard";
+import ArrowLink from "@/components/ArrowButton/ArrowLink";
+import ArrowButton from "@/components/ArrowButton/ArrowButton";
 
 export default function EditEvent() {
   const router = useRouter();
   const { id } = router.query;
   const { getEventByID, updateEvent } = useData();
   const { event, isLoadingEvent, errorEvent } = getEventByID(id);
+  const { data: session } = useSession();
+  const userId = session?.user.id;
 
   if (isLoadingEvent) {
     return <Loading />;
@@ -18,10 +24,23 @@ export default function EditEvent() {
   }
 
   return (
-    <EventFormContainer
-      title={"Event bearbeiten"}
-      updateDatabase={(eventData) => updateEvent(eventData, id)}
-      event={event}
-    />
+    <>
+      {userId === event.createdBy ? (
+        <EventFormContainer
+          title={"Event bearbeiten"}
+          updateDatabase={(eventData) => updateEvent(eventData, id)}
+          event={event}
+        />
+      ) : (
+        <MessageCard>
+          <p>Du bist nicht berechtigt, dieses Event zu bearbeiten</p>
+          {session ? (
+            <ArrowLink href="/events/new">Event Erstellen</ArrowLink>
+          ) : (
+            <ArrowButton onClick={() => signIn()}>Jetzt Anmelden</ArrowButton>
+          )}
+        </MessageCard>
+      )}
+    </>
   );
 }
