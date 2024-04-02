9069fcd35891543d66542859b6b88e531391e407
Bruno <bruno.dill@protonmail.com>
2024-04-02 13:06:47 +0200

    + create and style JoinButton component

---
+++ components/JoinButton/JoinButton.js
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import { StyledJoinButton } from "./JoinButton.styled";
+
+export default function JoinButton({ onJoinEvent, isAttendedByUser }) {
+  return (
+    <StyledJoinButton onClick={onJoinEvent}>
+      Teilnehmen
+      {isAttendedByUser ? (
+        <Image
+          src="/assets/icons/icon_heart-filled.svg"
+          alt="heart"
+          height={12}
+          width={14}
+        />
+      ) : (
+        <Image
+          src="/assets/icons/icon_heart.svg"
+          alt="heart"
+          height={12}
+          width={14}
+        />
+      )}
+    </StyledJoinButton>
+  );
+}
