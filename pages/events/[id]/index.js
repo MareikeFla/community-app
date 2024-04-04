52d3176312fee45a35ab21c4c48df7bac607d454
Bruno <bruno.dill@protonmail.com>
2024-04-04 10:43:50 +0200

    +add console logs for debugging

--- pages/events/[id]/index.js
+++ pages/events/[id]/index.js
@@ -6,7 +6,10 @@ import { useData } from "@/lib/useData";
 
 export default function EventDetailPage() {
   const router = useRouter();
+  console.log("events/id - before", router.query.id);
+
   const { getEventByID } = useData();
+  console.log("events/id - after", router.query.id);
   const { event, isLoadingEvent, errorEvent, mutateEvent } = getEventByID(
     router.query.id
   );
