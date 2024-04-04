07bdf44645234d25067c180cdfc1148701f036d4
Annette Wöhler <annettewoehler@gmx.de>
2024-04-04 12:59:34 +0200

    * Make SwitchButton reusable

--- components/SwitchButton/SwitchButton.js
+++ components/SwitchButton/SwitchButton.js
@@ -4,10 +4,10 @@ import {
   SwitchBackground,
   SwitchHandle,
 } from "./SwitchButton.styled";
-export default function SwitchButton({ toggleCosts, isChecked }) {
+export default function SwitchButton({ isChecked, onChange }) {
   return (
-    <SwitchContainer onClick={toggleCosts}>
-      <SwitchInput htmlFor="switch" $checked={isChecked} type="checkbox" />
+    <SwitchContainer onClick={onChange}>
+      <SwitchInput type="checkbox" checked={isChecked} onChange={() => {}} />
       <SwitchBackground $checked={isChecked} />
       <SwitchHandle $checked={isChecked} />
     </SwitchContainer>
