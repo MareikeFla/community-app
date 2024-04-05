4bdfe58536111888d21bb3bc5c500157fb57229f
Bruno <bruno.dill@protonmail.com>
2024-04-05 12:56:45 +0200

    - delete legacy prop

--- components/ReplyList/ReplyList.js
+++ components/ReplyList/ReplyList.js
@@ -1,13 +1,13 @@
 import Reply from "../Reply/Reply";
 import { List } from "./ReplyList.styled";
 
-export default function ReplyList({ replies, mutateEvent }) {
+export default function ReplyList({ replies }) {
   if (replies) {
     return (
       <List>
         {replies.map((reply) => (
           <li key={reply._id}>
-            <Reply reply={reply} mutateEvent={mutateEvent} />
+            <Reply reply={reply} />
           </li>
         ))}
       </List>
