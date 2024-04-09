7814e9d161ced4968cdc75eb4d2d576586df9995
Bruno <bruno.dill@protonmail.com>
2024-04-09 11:51:19 +0200

    + add button to edit comments

---
+++ components/EditCommentButton/EditCommentButton.styled.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const EditButton = styled.button`
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  justify-content: flex-end;
+`;
