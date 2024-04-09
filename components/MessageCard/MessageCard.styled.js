97eff6ddc8f72fd34edf33866ec865c32fff5728
Annette Wöhler <annettewoehler@gmx.de>
2024-04-09 11:31:32 +0200

    + Add spacings to MessageCards with button

--- components/MessageCard/MessageCard.styled.js
+++ components/MessageCard/MessageCard.styled.js
@@ -4,11 +4,17 @@ export const Card = styled.div`
   background-color: var(--color_white);
   border-radius: var(--border-radius_card);
   box-shadow: var(--shadow_card);
-  padding: ${(props) => (props.$pageNotFound ? "3rem 1.5rem 2.75rem" : "")};
+  padding: ${({ $hasButton }) =>
+    $hasButton ? "3rem 1.5rem" : "3rem 1.5rem 2.75rem"};
 `;
 
 export const Message = styled.h2`
   font: var(--font_heading-2);
   color: var(--color_night);
   text-align: center;
+
+  a,
+  button {
+    margin-top: 1rem;
+  }
 `;
