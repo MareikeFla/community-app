f3bd2aa5ff13ee2b5e38bd24719e7fd46dae713c
MareikeFla <mwidderich@gmail.com>
2024-04-10 09:09:30 +0200

    add profile page

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,8 +1,40 @@
-import { useSession } from "next-auth/react";
+import MessageCard from "@/components/MessageCard/MessageCard";
+import ArrowButton from "@/components/ArrowButton/ArrowButton";
+import Profile from "@/components/Profile/Profile";
+import ProfileForm from "@/components/Profile/ProfileForm";
+import { signIn, useSession, getSession } from "next-auth/react";
+import Loading from "@/components/Loading/Loading";
+
+import { useEffect, useState } from "react";
 
 export default function profilePage() {
-  const { data: session } = useSession();
-  const userId = session?.user.id;
+  let { data: session, status } = useSession();
+  const [editMode, setEditMode] = useState(false);
 
-  return null;
+  function toggleEditMode() {
+    setEditMode(!editMode);
+  }
+
+  if (status === "loading") {
+    return <Loading />;
+  }
+  if (!session) {
+    return (
+      <MessageCard>
+        <p>Du bist nicht berechtigt diese Seite zu besuchen.</p>
+        <ArrowButton onClick={() => signIn()}>Jetzt Anmelden</ArrowButton>
+      </MessageCard>
+    );
+  } else if (!editMode) {
+    return (
+      <Profile toggleEditMode={toggleEditMode} session={session}></Profile>
+    );
+  } else if (editMode) {
+    return (
+      <ProfileForm
+        toggleEditMode={toggleEditMode}
+        session={session}
+      ></ProfileForm>
+    );
+  }
 }
