6f1575c16ca90be69e877a49900575e52a485f0a
MareikeFla <mwidderich@gmail.com>
2024-04-12 22:15:28 +0200

    ...

--- pages/api/dailyTask/index.js
+++ pages/api/dailyTask/index.js
@@ -2,14 +2,29 @@
 
 export default async (req, res) => {
   if (req.method === "POST") {
-    // Your function logic here, e.g., interacting with MongoDB
-    console.log("Running daily task");
+    // Logic for POST request
+    console.log("Running POST daily task");
 
-    // Respond to the request
-    res.status(200).json({ message: "Daily task completed" });
+    // Example of a function that could interact with MongoDB
+    // await someDatabaseOperation();
+
+    res.status(200).json({ message: "POST daily task completed" });
+  } else if (req.method === "GET") {
+    // Logic for GET request
+    console.log("Running GET daily task");
+
+    // You can perform read-only operations here, for example:
+    // const data = await someDatabaseReadingOperation();
+
+    res.status(200).json({
+      message: "GET daily task completed",
+      data: {
+        /* some data */
+      },
+    });
   } else {
-    // Not allowed method
-    res.setHeader("Allow", ["POST"]);
+    // If the method is neither POST nor GET
+    res.setHeader("Allow", ["POST", "GET"]);
     res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 };
