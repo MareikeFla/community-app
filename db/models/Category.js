a23d8b02719448f1762419fdd32a0f5d80ef0204
Annette Wöhler <annettewoehler@gmx.de>
2024-04-17 19:41:15 +0200

    * Update Schema

--- db/models/Category.js
+++ db/models/Category.js
@@ -2,21 +2,21 @@ import mongoose from "mongoose";
 
 const { Schema } = mongoose;
 
-const subCategorySchema = new Schema({
-  title: String,
-  slug: String,
-  imageSource: String,
-  imageAlt: String,
-  parentCategory: { type: Schema.Types.ObjectId, ref: "Category" },
-});
-
 const categorySchema = new Schema({
   title: String,
   slug: String,
   imageSource: String,
   imageAlt: String,
   color: String,
-  subCategories: [subCategorySchema],
+  subCategories: [
+    {
+      title: String,
+      slug: String,
+      imageSource: String,
+      imageAlt: String,
+      parentCategory: { type: Schema.Types.ObjectId, ref: "Category" },
+    },
+  ],
 });
 
 const Category =
