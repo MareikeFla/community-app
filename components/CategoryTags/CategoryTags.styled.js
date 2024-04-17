27188003db3ecde2b1263e1b3c60c8ffb4e4385b
Annette Wöhler <annettewoehler@gmx.de>
2024-04-17 19:38:42 +0200

    + Add styling for category tag list

---
+++ components/CategoryTags/CategoryTags.styled.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const CategoryTagList = styled.div`
+  list-style: none;
+  display: inline-flex;
+  flex-wrap: wrap;
+  gap: 0.5rem 0.438rem;
+`;
+
+export const CategoryTag = styled.span`
+  font: var(--font_tag);
+  color: var(--color_white);
+  text-transform: uppercase;
+  background-color: ${({ color }) => `var(--color_${color})`};
+  border: none;
+  border-radius: var(--border-radius_button);
+  padding: 0.375rem 0.625rem 0.3125rem;
+`;
