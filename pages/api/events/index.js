22d34249dd4733e9837f97f028daaf05fc2d1bd6
MareikeFla <mwidderich@gmail.com>
2024-04-18 15:40:14 +0200

    Adapt Event API to fetch and update events based on meta info

---
+++ pages/api/events/index.js
@@ -0,0 +1,96 @@
+import dbConnect from "@/db/connect";
+import Event from "@/db/models/Event";
+import enrichEventObject from "@/lib/enrichEventObject";
+import {
+  fetchData,
+  upDateMetaInfo,
+  mergeCategories,
+  fetchUniqueEvents,
+  updateDatabase,
+  deleteEvent,
+  findUniqueEventsInFirstArray,
+} from "@/lib/daylieFetchHelper/functions";
+import { formateEvents } from "@/lib/daylieFetchHelper/formatingData";
+import {
+  ourDB,
+  metainfoUrl,
+  koelnDB,
+  koelnCategoriesUrl,
+  ourCategoriesUrl,
+  categoryRelation,
+} from "@/lib/daylieFetchHelper/variables";
+
+async function getEvents() {
+  const today = new Date().toISOString().split("T")[0];
+  const events = await Event.find({
+    "end.date": { $gte: today },
+  }).populate("category");
+  events.map((event) => enrichEventObject(event));
+  return events || [];
+}
+
+export default async function handler(request, response) {
+  await dbConnect();
+  const metainfo = await fetchData(metainfoUrl, ourDB);
+
+  if (metainfo.isUpToDate && request.method === "GET") {
+    try {
+      const events = await getEvents();
+      return response.status(200).json(events);
+    } catch (error) {
+      console.error(error);
+      return response.status(400).json({ error: error.message });
+    }
+  }
+
+  if (!metainfo.isUpToDate && request.method === "GET") {
+    try {
+      const koelnCategories = await fetchData(koelnCategoriesUrl, koelnDB);
+      const ourCategories = await fetchData(ourCategoriesUrl, ourDB);
+      const ourEvents = await getEvents();
+      const koelnEventsInOurDataBase = ourEvents.filter(
+        (event) => event.isFetchedEvent === true
+      );
+      const mergedCategories = mergeCategories(
+        ourCategories,
+        koelnCategories,
+        categoryRelation
+      );
+      const uniqueEvents = await fetchUniqueEvents(mergedCategories);
+      let deletedEvents = [];
+      let currentEvents = [];
+      const formatedEvents = formateEvents(uniqueEvents);
+      if (formatedEvents.length > 0) {
+        deletedEvents = findUniqueEventsInFirstArray(
+          koelnEventsInOurDataBase,
+          formatedEvents
+        );
+        if (deletedEvents.length > 0) {
+          deletedEvents.forEach((event) => deleteEvent(event._id));
+        }
+        currentEvents = findUniqueEventsInFirstArray(
+          formatedEvents,
+          deletedEvents
+        );
+        currentEvents.map((event) => updateDatabase(event));
+      }
+      upDateMetaInfo();
+
+      return response.status(200).json(currentEvents);
+    } catch (error) {
+      upDateMetaInfo();
+      response.status(400).json({ error: error.message });
+    }
+  }
+
+  if (request.method === "POST") {
+    try {
+      const event = request.body;
+      const newEvent = await Event.create(event);
+      response.status(201).json({ status: "Event created", id: newEvent._id });
+    } catch (error) {
+      console.error(error);
+      response.status(400).json({ error: error.message });
+    }
+  }
+}
