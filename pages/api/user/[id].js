3ce7d90ab145b0f7b1e241180617713400a24f8c
Bruno <bruno.dill@protonmail.com>
2024-04-18 10:15:03 +0200

    + add conditional to only allow Post and Patch requests for authorized users

--- pages/api/user/[id].js
+++ pages/api/user/[id].js
@@ -1,35 +1,41 @@
 import dbConnect from "@/db/connect";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../auth/[...nextauth]";
 import User from "@/db/models/User";
 
 export default async function handler(request, response) {
   await dbConnect();
-  const { userId, eventId } = request.body;
+  const session = await getServerSession(request, response, authOptions);
 
-  if (request.method === "POST") {
-    const userData = request.body;
-    const id = userData.id;
-    await User.findByIdAndUpdate(id, userData);
-    return response.status(200).json({ status: `User ${id} updated!` });
-  }
+  if (session) {
+    const { userId, eventId } = request.body;
 
-  if (request.method === "PATCH") {
-    try {
-      const user = await User.findById(userId);
-      if (!user) {
-        return response.status(404).json({ status: "User not found" });
+    if (request.method === "POST") {
+      const userData = request.body;
+      const id = userData.id;
+      await User.findByIdAndUpdate(id, userData);
+      return response.status(200).json({ status: `User ${id} updated!` });
+    }
+
+    if (request.method === "PATCH") {
+      try {
+        const user = await User.findById(userId);
+        if (!user) {
+          return response.status(404).json({ status: "User not found" });
+        }
+
+        if (user.attendedEvents.includes(eventId)) {
+          user.attendedEvents.pull(eventId);
+        } else {
+          user.attendedEvents.push(eventId);
+        }
+
+        await user.save();
+
+        return response.status(200).json({ status: `User updated!` });
+      } catch (error) {
+        return response.status(500).json({ status: "Internal Server Error" });
       }
-
-      if (user.attendedEvents.includes(eventId)) {
-        user.attendedEvents.pull(eventId);
-      } else {
-        user.attendedEvents.push(eventId);
-      }
-
-      await user.save();
-
-      return response.status(200).json({ status: `User updated!` });
-    } catch (error) {
-      return response.status(500).json({ status: "Internal Server Error" });
     }
   }
 }
