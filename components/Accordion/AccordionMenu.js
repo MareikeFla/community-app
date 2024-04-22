ed6a99a1ff009ead1c23fc70b8d47ebdbb3929da
MareikeFla <148346025+MareikeFla@users.noreply.github.com>
2024-04-22 11:38:48 +0200

    Update components/Accordion/AccordionMenu.js
    Co-authored-by: Niklas Röttger <98757391+NikRoe@users.noreply.github.com>

--- components/Accordion/AccordionMenu.js
+++ components/Accordion/AccordionMenu.js
@@ -33,7 +33,7 @@ export default function AccordionMenu({ sections = [] }) {
               newStateObject[id] = !isOpen;
               setAccordionSectionsIsOpen(newStateObject);
             }}
-          ></AccordionSection>
+          />
         );
       })}
     </AccordionWrap>
