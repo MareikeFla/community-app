4bdbc05a9dd8fd014edadabf83f0e248a3bd66a5
Bruno <bruno.dill@protonmail.com>
2024-04-22 12:53:36 +0200

    * changed prop name

--- components/Loading/Loading.js
+++ components/Loading/Loading.js
@@ -1,5 +1,5 @@
 import { Spinner } from "./Loading.styled";
 
-export default function Loading({ $small }) {
-  return <Spinner $small={$small}></Spinner>;
+export default function Loading({ small }) {
+  return <Spinner $small={small}></Spinner>;
 }
