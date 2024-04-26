97aa0c9269838f5af25507246f7605bff95c89da
Bruno <bruno.dill@protonmail.com>
2024-04-26 11:46:14 +0200

    + add logic to delete image on event deletion

--- pages/api/events/[id]/index.js
+++ pages/api/events/[id]/index.js
@@ -4,6 +4,7 @@ import Comment from "@/db/models/Comment";
 import enrichEventObject from "@/lib/enrichEventObject";
 import { getSession } from "next-auth/react";
 import User from "@/db/models/User";
+import cloudinary from "cloudinary";
 
 export default async function handler(request, response) {
   await dbConnect();
@@ -46,15 +47,40 @@ export default async function handler(request, response) {
         return response.status(404).json({ status: "Event not found" });
       }
 
+      let imageDeleted = true;
+
+      if (event.image.public_id) {
+        try {
+          cloudinary.v2.config({
+            cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+            api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
+            api_secret: process.env.NEXT_PUBLIC_CLOUDINARY_API_SECRET,
+          });
+
+          await cloudinary.v2.uploader.destroy(event.image.public_id);
+        } catch (error) {
+          console.error("Error deleting image:", error);
+          imageDeleted = false;
+        }
+      }
+
       if (event.comments?.length) {
         await Comment.deleteMany({ _id: { $in: event.comments } });
       }
 
       await Event.findByIdAndDelete(id);
 
-      response.status(200).json({
-        status: `Event ${id} and related comments successfully deleted.`,
-      });
+      if (imageDeleted) {
+        return response.status(200).json({
+          success: true,
+          status: `Event ${id} and related image successfully deleted.`,
+        });
+      } else {
+        return response.status(500).json({
+          success: false,
+          error: "Error deleting event or image",
+        });
+      }
     } catch (error) {
       response.status(400).json({ error: error.message });
     }
@@ -64,5 +90,22 @@ export default async function handler(request, response) {
     const eventData = request.body;
     await Event.findByIdAndUpdate(id, eventData);
     return response.status(200).json({ status: `Event ${id} updated!` });
+  }
+
+  if (request.method === "PATCH") {
+    try {
+      const updatedEvent = await Event.findByIdAndUpdate(
+        id,
+        { $unset: { image: "" } },
+        { new: true }
+      );
+      if (!updatedEvent) {
+        return response.status(404).json({ error: "Event not found" });
+      }
+
+      return response.status(200).json({ status: "Image deleted from event" });
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
   }
 }
