97afc74c20c33ea966e4a3dbadaa0bf30d2fe14a
MareikeFla <mwidderich@gmail.com>
2024-05-03 14:04:45 +0200

    cleaning up to improve redabillity

--- pages/search/index.js
+++ pages/search/index.js
@@ -3,8 +3,10 @@ import EventList from "@/components/EventList/EventList";
 import useSearch from "@/lib/useSearch";
 import { useEffect, useState } from "react";
 import { useData } from "@/lib/useData";
+import Loading from "@/components/Loading/Loading";
+import Error from "next/error";
 
-function isAnyValueTrue(objOne, objTwo) {
+function isAnyValueTrue(objOne = {}, objTwo = {}) {
   let result = false;
   for (const key in objOne) {
     if (objOne[key] === true) {
@@ -50,13 +52,13 @@ export default function SearchPage() {
       : filteredEvents.events.length;
     const searchPerformed = filteredEvents?.hasResults !== undefined;
     const hasResults = eventCount > 0;
+    const searchString = searchPerformed ? ` für "${searchTerm}"` : "";
 
     if (!searchPerformed && !isFiltered) {
       setTitle(null);
       return;
     }
 
-    const searchString = searchPerformed ? ` für "${searchTerm}"` : "";
     setTitle(
       hasResults
         ? `Deine Suchergebnisse${searchString} (${eventCount})`
@@ -65,24 +67,27 @@ export default function SearchPage() {
   }, [filteredEvents?.hasResults, events, isFiltered]);
 
   // Reset the filter when a new searchTerm is submitted
+
   useEffect(() => {
     setA11yFilter({});
     setCategoryFilter({});
+    setIsFiltered(false);
   }, [searchTerm]);
 
-  // Filter events based on a11yIcon selection
+  // Filter events based on filter selection
+
   useEffect(() => {
     const hasCategoryFilters = isAnyValueTrue(categoryFilter);
-    const hasa11yFilters = isAnyValueTrue(a11yFilter);
-    let currentEvents = [...filteredEvents.events];
-    if (
-      filteredEvents.hasResults === undefined &&
-      (hasCategoryFilters || hasa11yFilters)
-    ) {
-      currentEvents = fetchedEvents;
-    }
-    if (hasa11yFilters === true) {
-      let currentEventsFiltered = currentEvents.filter((event) => {
+    const hasA11yFilters = isAnyValueTrue(a11yFilter);
+    const hasFilters = hasCategoryFilters || hasA11yFilters;
+    const searchPerformed = filteredEvents?.hasResults !== undefined;
+
+    let newFilteredEvents = searchPerformed
+      ? [...filteredEvents.events]
+      : [...fetchedEvents];
+
+    if (hasA11yFilters === true) {
+      let filteredResult = newFilteredEvents.filter((event) => {
         let isIncluded = false;
         const a11yIcons = event.a11yIcons || [];
         a11yIcons.forEach((id) => {
@@ -92,30 +97,31 @@ export default function SearchPage() {
         });
         return isIncluded;
       });
-      currentEvents = currentEventsFiltered;
+      newFilteredEvents = filteredResult;
     }
 
     if (hasCategoryFilters === true) {
-      const result = currentEvents.filter(
+      const result = newFilteredEvents.filter(
         (event) => categoryFilter[event.category._id] === true
       );
-      currentEvents = result;
+      newFilteredEvents = result;
     }
-    if (
-      filteredEvents.hasResults === undefined &&
-      (hasCategoryFilters || hasa11yFilters) &&
-      currentEvents.length > 0
-    ) {
-      setTitle(`Deine Suchergebnisse (${currentEvents.length})`);
+    // To prevent the title from appearing with delay
+    if (!searchPerformed && hasFilters && newFilteredEvents.length > 0) {
+      setTitle(`Deine Suchergebnisse (${newFilteredEvents.length})`);
     }
 
-    setEvents(currentEvents);
+    setEvents(newFilteredEvents);
     setIsFiltered(isAnyValueTrue(a11yFilter, categoryFilter));
   }, [a11yFilter, categoryFilter]);
 
-  if (isLoadingEvents || errorEvents) {
-    return;
+  if (isLoadingEvents) {
+    return <Loading />;
   }
+  if (errorEvents) {
+    return <Error />;
+  }
+
   return (
     <>
       <SearchCard
